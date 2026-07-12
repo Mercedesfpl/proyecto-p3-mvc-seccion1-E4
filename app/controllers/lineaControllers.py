61c4ec5c986b25a66c8657bc09adea3ce6680094
Jeremy <isapartarn03@gmail.com>
2026-07-12 17:51:11 -0400

    features: Modificación de lineaControllers para que el controlador contenga menos logica. Creación de la carpeta repositories. Creación de los archivos lineaRepository y lineaService

--- app/controllers/lineaControllers.py
+++ app/controllers/lineaControllers.py
@@ -1,138 +1,84 @@
 # app/controllers/lineaControllers.py
-from ..services.linea_factory import LineaFactory
-from ..models.exceptions import ResourceNotValid, ResourceNotFound
-from ..helpers.makeResponse import success_response, error_response
-from flask import jsonify
-
-from ..database.connection import (
-    get_all_lineas,
-    get_linea_by_id,
-    get_personas_all,
-    obtener_usuario_por_id,
-    agregar_elemento,
-    get_persona_by_id,
-    guardar_datos,
-    get_users_by_rol,
-)
-
+from flask import jsonify, request
+from app.services.lineaService import LineaServices
+from app.models.exceptions import ResourceNotFound, ResourceNotValid
+from app.helpers.makeResponse import success_response, error_response
 
 def get_lineas():
-    """Obtener todas las líneas (no suspendidas)"""
-    lineas = get_all_lineas()
-    resultado = [
-        {
-            "id": l.id,
-            "nombre": l.linea_nombre,
-            "presidente": l.presidente_nombre,
-            "secretario_nombre": l.secretario_nombre,
-            "rif": l.rif,
-        }
-        for l in lineas
-    ]
-    return success_response(data=resultado)
-
-
+    #Obtener todas las líneas (no suspendidas)
+    try:
+        data = LineaServices.get_all_lineas()
+        return success_response(data=data)
+    except Exception as e: 
+        return error_response(error=str(e), message="Error en obtener las líneas", status_code=500)
+    
 def get_linea_by_id(id_linea):
-    """Obtener una línea por ID"""
-    lineas = get_linea_by_id(id_linea=id_linea)
-    resultado = [{"id": l.id, "nombre": l.nombre} for l in lineas]
-    return success_response(resultado)
-
-
+    #Obtener una línea por id
+    try:
+        data = LineaServices.get_linea_by_id(id_linea)
+        return success_response(data=data)
+    except ResourceNotFound as e: 
+        return error_response(error=str(e), message="Línea no encontrada", status_code=404)
+    except Exception as e:
+        return error_response(error=str(e), message="Error al obtener la línea", status_code=500)
+    
 def get_personas_disponibles():
-    """Obtener todas las personas para los selects"""
+    #Obtener todas las personas (No suspendidas)
     try:
-        usuarios = get_personas_all()
-
-        if not usuarios:
-            # Opción A: Retornar lista vacía con código 200
-            raise ResourceNotFound(nombre_del_recurso="Personas")
-            # Opción B: Retornar un mensaje informativo
-            # return jsonify({"message": "No hay usuarios registrados"}), 404
-
-        # Transformar la lista de objetos a un formato JSON serializable
-        resultado = [
-            {"id": u.id, "nombre": u.nombre, "cedula": u.cedula, "rol": u.rol}
-            for u in usuarios
-        ]
-        return success_response(resultado)
-
+        data = LineaServices.get_personas_disponibles()
+        return success_response(data=data)
+    except ResourceNotFound as e:
+        return success_response(data=[]) #Array vacio cuando no hay nada jijiji
     except Exception as e:
-        # Valida si hubo un error de conexión o de base de datos
-        return error_response(
-            error=str(e), message="Ha ocurrido un fallo inesperado", status_code=500
-        )
-
-
-def create_linea(data):
-
-    # Delegar creación a la fábrica
-    nueva_linea = LineaFactory.crear_linea(data)
-    # Guardar
-    if agregar_elemento(
-        nueva_linea
-    ):  # asumo que esta función guarda y retorna True/False
-        return success_response(
-            message="Línea creada exitosamente", data=nueva_linea.to_dict()
-        )
-    else:
-        return error_response(message="error al guardar el recurso", status_code=503)
-
-
-def update_linea(id_linea, data):
-    """Actualizar una línea existente"""
+        return error_response(error=str(e), message="Error en obtener las personas disponibles", status_code=500)
+    
+def get_secretarios_disponibles():
+    #Obtener todos los secretarios (No suspendidas)
     try:
-        print("Datos recibidos para actualizar:", data)
-
-        linea = get_linea_by_id(id_linea)
-        if not linea or linea.suspendido:
-            raise ResourceNotFound(nombre_del_recurso="Linea")
-
-        # Actualizar campos
-        if "nombre" in data:
-            linea.nombre = data["nombre"].strip()
-        if "rif" in data:
-            linea.rif = data["rif"].strip()
-        if "presidente_id" in data:
-            presidente = get_persona_by_id(data["presidente_id"])
-            if not presidente:
-                raise ResourceNotFound("Presidente")
-            linea.presidente_id = data["presidente_id"]
-        if "secretario_id" in data:
-            if data["secretario_id"]:
-                secretario = obtener_usuario_por_id(data["secretario_id"])
-                if not secretario:
-                    raise ResourceNotFound("Secretario")
-            linea.secretario_id = data["secretario_id"]
-
-        if guardar_datos():
-            return success_response(
-                message="Línea actualizada exitosamente", data=linea.to_dict()
-            )
-
+        data = LineaServices.get_secretarios_disponibles()
+        return success_response(data=data)
+    except ResourceNotFound as e:
+        return success_response(data=[]) #Array vacio cuando no hay nada jijiji
     except Exception as e:
-        return jsonify({"error": f"Error al actualizar línea: {str(e)}"}), 500
+        return error_response(error=str(e), message="Error en obtener los secretarios disponibles", status_code=500)
+    
+def create_linea():
+    #Crea una línea
 
+    try: 
+        data = request.get_json()
+        linea = LineaServices.create_linea(data)
+        return success_response(message="Línea creada existosamente", data=linea.to_dict())
+    except ResourceNotValid as e:
+        return error_response(error=str(e), message=str(e), status_code=400)
+    except ResourceNotFound as e:
+        return error_response(error=str(e), message=str(e), status_code=404)
+    except Exception as e:
+        return error_response(error=str(e), message="Error en crear una línea", status_code=500)
+    
+def update_linea(id_linea):
+    #Actualizar linea 
+
+        try: 
+            data = request.get_json()
+            linea = LineaServices.update_linea(id_linea ,data)
+            return success_response(message="Línea actualizada exitosamente", data=linea.to_dict())
+        except ResourceNotValid as e:
+            return error_response(error=str(e), message=str(e), status_code=400)
+        except ResourceNotFound as e:
+            return error_response(error=str(e), message="Línea no encontrada", status_code=404)
+        except Exception as e:
+            return error_response(error=str(e), message="Error en actualizar una línea", status_code=500)
 
 def delete_linea(id_linea):
-    """Suspender una línea (no se elimina físicamente)"""
-    try:
-        linea = get_linea_by_id(id_linea=id_linea)
-        if not linea:
-            raise ResourceNotFound("Linea")
+    #"Eliminar" (Suspender) una línea existente
 
-        linea.suspendido = True
-        if guardar_datos():
-            return success_response(message="Línea suspendida exitosamente")
-
+    try: 
+        LineaServices.delete_linea(id_linea)
+        return success_response(message="Línea eliminada de manera exitosa")
+    except ResourceNotFound as e:
+        return error_response(error=str(e), message="Línea no encontrada", status_code=404)
     except Exception as e:
-
-        return jsonify({"error": f"Error al suspender línea: {str(e)}"}), 500
-
-
-def get_secretarios_disponibles():
-    personas = get_users_by_rol(rol="secretario")
-    resultado = [
-        {"id": p.id, "nombre": p.nombre, "apellido": p.apellido} for p in personas
-    ]
-    return success_response(data=resultado)
+        return error_response(error=str(e), message="Error en eliminar una línea", status_code=500)
+    
+    
---
+++ app/repositories/lineaRepository.py
@@ -0,0 +1,38 @@
+from app.models.linea import linea
+from extensions import db
+
+class LineaRepository:
+    #Maneja todas las consultas a la base de datos para Líneas
+
+    @staticmethod
+    def get_all():
+        return linea.query.filter_by(suspendido=False).all()
+    
+    @staticmethod
+    def get_by_id(id_linea):
+        return linea.query.get(id_linea)
+    
+    @staticmethod
+    def existentePorNombre(nombre, exclude_id=None):
+        query = linea.query.filter_by(nombre=nombre)
+        if exclude_id:
+            query = query.filter(linea.id != exclude_id)
+        return query.first() is not None
+
+    @staticmethod
+    def existentePorRif(rif, exclude_id=None):
+        query = linea.query.filter_by(rif=rif)
+        if exclude_id:
+            query = query.filter(linea.id != exclude_id)
+        return query.firts() is not None
+    
+    @staticmethod
+    def update(linea):
+        db.session.commit()
+        return linea
+    
+    @staticmethod
+    def delete(linea):
+        linea.suspendido = True
+        db.session.commit()
+        return linea
